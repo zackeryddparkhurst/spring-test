8c9066893d3a65d201d5a93f8a45fbf188233354
Zackery Parkhurst <zackery.parkhurst@experianhealth.com>
2017-01-11 09:50:32 -0600

    copied johoo exactly

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,24 +1,26 @@
 package hello;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import  org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @Value("${hello.message}")
     private String message;
 
-    @Autowired
-    private DiscoveryClient discoveryClient;
-
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!" + " hello from spring boot" + message;
+        System.out.println("test:" + discoveryClient.getServices());
+        return "Greetings from Spring Boot! - It is " + message;
     }
-    
+
+
 }
