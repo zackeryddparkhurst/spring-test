d4a6811ba9a7b25f6dd66d58d81893037b9286b3
Zackery Parkhurst <zackery.parkhurst@passporthealth.com>
2017-01-11 08:24:09 -0600

    test

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,13 +1,14 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    //@Value("${hello.message}")
-    //private String message;
+    @Value("${hello.message}")
+    private static String message;
 
     @RequestMapping("/")
     public String index() {
