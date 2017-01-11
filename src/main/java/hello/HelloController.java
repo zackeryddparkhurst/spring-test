f77668e9c4a639a2668de20d571400fbb1b88acf
Zackery Parkhurst <zackery.parkhurst@experianhealth.com>
2017-01-11 09:42:45 -0600

    added back discovery client

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,6 +1,8 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +12,9 @@ public class HelloController {
 
     @Value("${hello.message}")
     private String message;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @RequestMapping("/")
     public String index() {
