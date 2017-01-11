a90764de675252c8ed8d58c0f86c072f463bf620
Zackery Parkhurst <zackery.parkhurst@passporthealth.com>
2017-01-11 08:35:13 -0600

    discoverty client

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,20 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+
     @Value("${hello.message}")
     private String message;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @RequestMapping("/")
     public String index() {
