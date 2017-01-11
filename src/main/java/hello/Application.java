fd0bdda52785e2d5f2e2efbb9298b6523d89cd34
Zackery Parkhurst <zackery.parkhurst@experianhealth.com>
2017-01-10 20:03:25 -0600

    put bean definition back in

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,7 +1,12 @@
 package hello;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
@@ -10,7 +15,7 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    /*@Bean
+    @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
 
@@ -23,6 +28,6 @@ public class Application {
             }
 
         };
-    }*/
+    }
 
 }
