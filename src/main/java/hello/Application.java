e844058e6b3bdd2851780151ffd43321bac7c59c
Zackery Parkhurst <zackery.parkhurst@experianhealth.com>
2017-01-10 19:55:57 -0600

    commented out bean definition

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,12 +1,7 @@
 package hello;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -15,7 +10,7 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
+    /*@Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
 
@@ -28,6 +23,6 @@ public class Application {
             }
 
         };
-    }
+    }*/
 
 }
