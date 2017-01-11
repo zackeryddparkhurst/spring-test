009213ac41836565485c57b4371bd9170c70bc82
Zackery Parkhurst <zackery.parkhurst@passporthealth.com>
2017-01-11 08:21:01 -0600

    stuff

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,18 +1,17 @@
 package hello;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @Value("${hello.message}")
-    private static String message;
+    //@Value("${hello.message}")
+    //private String message;
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!" + message;
+        return "Greetings from Spring Boot!" + " hello from spring boot";
     }
     
 }
