af893fd2531be88eb07effc509ebee0f46894839
Zackery Parkhurst <zackery.parkhurst@experianhealth.com>
2017-01-11 10:17:17 -0600

    test

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -3,7 +3,6 @@ package hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import  org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
